c40f841e5e48f1fc221446c595b3990a02761b30
Udochi Okeke <udochio@hotmail.com>
2025-02-19 00:47:59 -0500

    built romance quiz app

---
+++ romance-vs-crypto/src/app/components/Demo.tsx
@@ -0,0 +1,9 @@
+export default function Demo() {
+    return (
+      <div className="w-[300px] mx-auto py-4 px-2">
+        <h1 className="text-2xl font-bold text-center mb-4">Romance or HODL Quiz</h1>
+        <p id="description">Loading quiz...</p>
+    <div id="quiz"></div>
+      </div>
+    );
+  }
